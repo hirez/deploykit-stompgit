995478dfc712876fd43e0e7a22e7e0ef22e115fc
John Hawkes-Reed <john.hawkes-reed@futurenet.com>
2014-05-21 16:28:00 +0100

    A hateful build-script.

---
+++ jenkins-build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+mkdir -p build/usr/sbin/
+mkdir -p build/etc/stomp-git/
+mkdir -p build/etc/init.d/
+
+cp stomp-git build/usr/sbin/stomp-git
+cp stomp-git.yaml build/etc/stomp-git/stomp-git.yaml
+cp stomp-git.initscript build/etc/init.d/stomp-git
+
+PKG_VER=`grep '^version' stomp-git | awk -F\" '{print $2}'`
+BUILDN=${BUILD_NUMBER:=1}
+
+/usr/bin/fakeroot /usr/local/bin/fpm -s dir -t deb -n "stomp-git" -f \
+  -v ${PKG_VER}.${BUILDN} --description "Future remote repo tracker" \
+  -a all -m "<list.itoperations@futurenet.com>" \
+  -C ./build .
+
