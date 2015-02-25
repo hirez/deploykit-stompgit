6a728332662966e2c2df01ba7363e5f9b74d6996
John Hawkes-Reed <john.hawkes-reed@futurenet.com>
2015-02-25 16:43:48 +0000

    Attempt to build it right. Or at least less wrong.

--- jenkins-build.sh
+++ jenkins-build.sh
@@ -3,8 +3,10 @@
 mkdir -p build/usr/sbin/
 mkdir -p build/etc/stomp-git/
 mkdir -p build/etc/init.d/
+mkdir -p build/usr/lib/ruby/vendor_ruby
 
 cp stomp-git build/usr/sbin/stomp-git
+cp lib/stomp-lib.rb build/usr/lib/ruby/vendor_ruby
 cp stomp-git.yaml build/etc/stomp-git/stomp-git.yaml
 cp stomp-git.initscript build/etc/init.d/stomp-git
 
